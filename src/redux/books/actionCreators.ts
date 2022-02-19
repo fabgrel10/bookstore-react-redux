1af21cb934c9585b37faef5e0afa17d089a488dd
Fabricio Garcia <fabgrel10@gmail.com>
2022-02-19 14:23:46 -0600

    ♻ refactor: Change type for actions's parameters to Book

--- src/redux/books/actionCreators.ts
+++ src/redux/books/actionCreators.ts
@@ -1,13 +1,13 @@
 import { ADD_BOOK, REMOVE_BOOK } from './actionTypes';
 
-export const addBook = (payload: Data) => {
+export const addBook = (payload: Book) => {
   return {
     type: ADD_BOOK,
     payload
   };
 };
 
-export const removeBook = (payload: Data) => {
+export const removeBook = (payload: Book) => {
   return {
     type: REMOVE_BOOK,
     payload
